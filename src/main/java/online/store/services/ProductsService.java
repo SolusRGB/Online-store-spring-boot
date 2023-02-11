13bf37abe537939723725b22bff2c7d3fcea5717
SolusRGB <brandoneddycode@gmail.com>
2023-02-11 17:52:27 +0000

    import - List/Collectors create - getAllProducts, getProductsByCategory, getDealsOfTheDay, getAllSupportedCategories

--- src/main/java/online/store/services/ProductsService.java
+++ src/main/java/online/store/services/ProductsService.java
@@ -1,11 +1,14 @@
 package online.store.services;
 
+import online.store.model.Product;
 import online.store.repositories.ProductCategoryRepository;
 import online.store.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * @author Michael Pogrebinsky - www.topdeveloperacademy.com
  * Integrates with the database API and handles products and categories business logic
  */
 @Service
@@ -17,4 +20,24 @@ public class ProductsService {
         this.productRepository = productRepository;
         this.productCategoryRepository = productCategoryRepository;
     }
+
+    public List<String> getAllSupportedCategories() {
+        return productCategoryRepository.findAll()
+                .stream()
+                .map(productCategory -> productCategory.getCategory())
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> getDealsOfTheDay(int atMostNumberOfProducts) {
+        return productRepository.findAtMostNumberOfProducts(atMostNumberOfProducts);
+    }
+
+    public List<Product> getProductsByCategory(String productCategory) {
+        return productRepository.findByCategory(productCategory);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
 }
