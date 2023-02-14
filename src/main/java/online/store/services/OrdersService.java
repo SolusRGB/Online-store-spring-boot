1d5d6d082d7fdf33dbf1b7ded074fd62103192d1
SolusRGB <brandoneddycode@gmail.com>
2023-02-14 18:07:33 +0000

    create - OrdersService
    Takes a collection of orders and saves them in the DB "Orders" table

--- src/main/java/online/store/services/OrdersService.java
+++ src/main/java/online/store/services/OrdersService.java
@@ -1,4 +1,18 @@
 package online.store.services;
 
+import online.store.model.Order;
+import online.store.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrdersService {
+    private final OrderRepository orderRepository;
+
+    public OrdersService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void placeOrders(Iterable<Order> orders) {
+        orderRepository.saveAll(orders);
+    }
 }
