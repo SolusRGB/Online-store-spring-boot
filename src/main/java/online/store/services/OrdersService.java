80d8308db7d198e83520aab8af5543028b1c5db1
SolusRGB <brandoneddycode@gmail.com>
2023-02-14 19:09:23 +0000

    Implemented OrdersService class for handling order placement operations and validating the number of items ordered. The maximum number of items 10) allowed for an order can be configured through the application properties. #orderservice #validation #spring

--- src/main/java/online/store/services/OrdersService.java
+++ src/main/java/online/store/services/OrdersService.java
@@ -2,17 +2,36 @@ package online.store.services;
 
 import online.store.model.Order;
 import online.store.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class OrdersService {
+
+    private final long maxNumberOfItems;
     private final OrderRepository orderRepository;
 
-    public OrdersService(OrderRepository orderRepository) {
+    public OrdersService(OrderRepository orderRepository,
+                         @Value("${products.service.max-number-of-items:25}") long maxNumberOfItems) {
         this.orderRepository = orderRepository;
+        this.maxNumberOfItems = maxNumberOfItems;
+    }
+
+    private void validateNumberOfItemsOrdered(Iterable<Order> orders) {
+        long totalNumberOfItems = 0;
+        for (Order order: orders)  {
+            totalNumberOfItems += order.getQuantity();
+        }
+        if (totalNumberOfItems > maxNumberOfItems) {
+            throw new IllegalStateException(String.format("Number of products %d exceeded the limit of %d",
+                    totalNumberOfItems, maxNumberOfItems));
+        }
     }
 
     public void placeOrders(Iterable<Order> orders) {
+        validateNumberOfItemsOrdered(orders);
+
         orderRepository.saveAll(orders);
     }
 }
